103588c70887a39907c50f4cd2932ce73318f215
BunjanMark <bunjan.mark476@gmail.com>
2023-09-10 17:30:06 +0800

    First commit

---
+++ AwesomeProject/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Text, View, Button} from 'react-native';
+
+let name = 'Shosho';
+let name2 = 'Bunjan';
+const App = () => {
+  let x = 2000;
+  let y = 200;
+  let age = 11;
+
+  return (
+    <View>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello worldddd</Text>
+      <Button title="Press me" />
+      <View>
+        <Text>{name}</Text>
+        <Text>
+          Hello worlddd, this isss {name} {name2}
+        </Text>
+
+        {age >= 20 ? (
+          <Text style={{fontSize: 40, color: 'red'}}>Youre shit 20 above</Text>
+        ) : (
+          <Text style={{color: 'blue'}}>Youre underage</Text>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default App;
